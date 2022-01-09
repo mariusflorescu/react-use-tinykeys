547edb3a2e06ad467bacbb83700a9f3647b3c82b
Marius Florescu <mariusflorescu23@icloud.com>
2022-01-10 00:28:31 +0200

    trying to fix window issue

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import type { KeyBindingMap, KeyBindingOptions } from './types';
 import tinykeys from './tinykeys';
 
 const useTinykeys = (
-  target: Window | HTMLElement,
   keyBindingMap: KeyBindingMap,
   options: KeyBindingOptions = {}
 ) => {
   React.useEffect(() => {
-    const unsub = tinykeys(target, keyBindingMap, options);
+    if (typeof window === undefined) return;
+
+    const unsub = tinykeys(window, keyBindingMap, options);
     return () => unsub();
   }, [keyBindingMap]);
   return;
